1d13f67153146675f802e37ebda9e040aba5cd2a
Michelle Macharia <1234.mich.mach@gmail.com>
2026-06-15 20:50:21 +0000

    Users of the library application

---
+++ Library/users.py
@@ -0,0 +1,7 @@
+class Users():
+
+    def __init__(self, user_id, name, email, borrowed_books):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.borrowed_books = borrowed_books
