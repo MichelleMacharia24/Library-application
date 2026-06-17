d2f2ef03a353f9135caa0effcc366b48a3de42a7
Michelle Macharia <1234.mich.mach@gmail.com>
2026-06-17 16:45:32 +0000

    Books in the library

--- Library/books.py
+++ Library/books.py
@@ -4,15 +4,48 @@ class Books():
         self.book_id = book_id
         self.title = title
         self.author = author
-        self.available = available
+        self.available = True
 
     def book_info(self):
 
-        """
-        Displays the information about the book.
-        """
+        """Displays the information about the book."""
 
         print(f"Book ID: {self.book_id}\n"
               f"Title: {self.title}\n"
               f"Author: {self.author}\n"
               f"Availability: {self.available}")
+        
+
+    def book_borrowed(self):
+
+        """Marks a book as borrowed."""
+
+        if self.available == True:
+            print(f"{self.title} is available")
+        else:
+            print(f"{self.title} is already borrowed.")
+
+    def book_returned(self):
+
+        """Marks a book as returned."""
+
+        if self.available == True:
+            print(f"{self.title} has been returned.")
+
+    def update_book(self, new_title, new_author):
+
+        """Updates the details of an exisiting book."""
+
+        self.title = new_title
+        self.author = new_author
+
+        print("Book details updated.")
+
+
+book1 = Books(10, "Python rules", "Michelle Macharia", True)       
+
+print(book1.book_info())
+
+
+
+
