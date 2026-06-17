708cdc395f10a6581f991253dc4465dc00298713
Michelle Macharia <1234.mich.mach@gmail.com>
2026-06-17 21:03:38 +0000

    Testing library system

--- Library/test.py
+++ Library/test.py
@@ -1,5 +1,6 @@
 from users import Users
 from books import Books
+from borrowed import Borrowed
 
 user1 = Users(1, "Alice Wonder", "alicewonder@python.com")
 
@@ -10,3 +11,11 @@ user1.borrow_book(book1)
 user1.borrow_book(book2)
 
 user1.user_info()
+
+
+record1 = Borrowed(1,50,101)
+record1.new_record("20/06/2026")
+record1.close_record("01/02/2027")
+
+record1.record_info()
+print(record1.check_returned())
