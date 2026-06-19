17565b23a3135fcb777847e380665244879716bd
Michelle Macharia <1234.mich.mach@gmail.com>
2026-06-19 17:16:09 +0000

    Library system controller

--- Library/library.py
+++ Library/library.py
@@ -1,6 +1,6 @@
 class Library():
 
     def __init__(self, books, users, records):
-        self.books = books
-        self.users = users
-        self.records = records
+        self.books = []
+        self.users = []
+        self.records = []
