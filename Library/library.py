04c5c881ee0ee8a9d6b5ef2747bb2efc00fa00c6
Michelle Macharia <1234.mich.mach@gmail.com>
2026-06-22 15:44:36 +0000

    Library system controller

--- Library/library.py
+++ Library/library.py
@@ -35,10 +35,45 @@ class Library():
                 book.borrow_book()
                 user.borrow_book(book)
 
-                print(f"{user.name} has successfully borrowed {book.title}")
+                print(f"{user.name} has successfully borrowed {book.title}.")
 
             else:
                 print(f"{book.title} has already been borrowed.")
             
         else:
             print("This book is not in the library.")
+
+    def return_book(self, user, book):
+         
+         """Allows users to return books to the library."""
+         
+         if book in self.books:
+             
+             if not book.available:
+                book.return_book()
+                user.return_book(book)
+
+                print(f"{user.name} has successfully returned {book.title}.")
+             
+             else:
+                print(f"{book.title} is not currently borrowed.")
+
+         else:
+            print("This book is not in the library.")
+
+    def search_books(self, title):
+
+        """Allows the user or librarian to search for books by their title."""
+
+        found = False
+
+        for book in self.books:
+
+         if book.title.lower() == title.lower():
+            print("Book has been found:")
+            book.book_info()
+            found = True
+            break
+
+        if not found:
+            print("Book is not in library.")
